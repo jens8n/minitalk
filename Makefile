6afbd98424a29f83675f1b763a36c2ee45e021ae
jens8n <jensen.bucoy@gmail.com>
2022-11-04 15:44:20 +0400

    makefile test

--- Makefile
+++ Makefile
@@ -6,26 +6,24 @@
 #    By: jebucoy <jebucoy@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/07 18:15:10 by jebucoy           #+#    #+#              #
-#    Updated: 2022/10/27 20:50:14 by jebucoy          ###   ########.fr        #
+#    Updated: 2022/11/02 16:19:37 by jebucoy          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
-
-NAME	=	minitalk
-
-S_NAME	=	server
 
 CC		= 	gcc
 
 CFLAGS	= -Wall -Wextra -Werror -I ${HEADER}
 
+RM	=	rm -rf
+
 HEADER	=	inc
+
+S_NAME	=	server
 
 S_SRC	=	mandatory/server.c \
 
 S_OBJS	=	$(S_SRC:.c=.o)
 
-$(S_NAME)		:	$(S_OBJS)
-				$(CC) $(CFLAGS) $(S_OBJS) $(U_OBJS) -o $(S_NAME) 
 
 C_NAME	=	client
 
@@ -33,8 +31,6 @@ C_SRC	=	mandatory/client.c \
 
 C_OBJS	=	$(C_SRC:.c=.o)
 
-$(C_NAME)	:		$(C_OBJS)
-				$(CC) $(CFLAGS) $(C_OBJS) $(U_OBJS) -o $(C_NAME) 
 
 UTILS	=	utils/ft_atoi.c \
 			utils/ft_putchar_fd.c \
@@ -44,9 +40,13 @@ UTILS	=	utils/ft_atoi.c \
 
 U_OBJS	=	$(UTILS:.c=.o)
 
-RM	=	rm -rf
+all	:	$(U_OBJS) $(S_NAME) $(C_NAME)
 
-all :	$(NAME)
+$(S_NAME)		:	$(S_OBJS)
+				$(CC) $(CFLAGS) $(S_OBJS) $(U_OBJS) -o $(S_NAME) 
+
+$(C_NAME)	:		$(C_OBJS)
+				$(CC) $(CFLAGS) $(C_OBJS) $(U_OBJS) -o $(C_NAME)
 
 clean	:	
 		$(RM) $(S_OBJS) $(C_OBJS) $(U_OBJS)
--- mandatory/server.c
+++ mandatory/server.c
@@ -6,7 +6,7 @@
 /*   By: jebucoy <jebucoy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 15:58:59 by jebucoy           #+#    #+#             */
-/*   Updated: 2022/10/27 18:32:50 by jebucoy          ###   ########.fr       */
+/*   Updated: 2022/11/02 19:26:07 by jebucoy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,6 @@ void	convert_tochar(int sig)
 		if (sig == SIGUSR2)
 		{
 			c = c | (1 << i);
-			printf("1");
-		}
-		if (sig == SIGUSR1)
-		{
-			printf("0");
 		}
 		i++;
 		if (i == 8)
@@ -34,7 +29,6 @@ void	convert_tochar(int sig)
 			ft_putchar_fd(c, 1);
 			c = 0;
 			i = 0;
-			// printf("\n");
 		}
 	}
 }
@@ -42,17 +36,24 @@ void	convert_tochar(int sig)
 int	main(void)
 {
 	pid_t	pid;
+	char	*msg;
+	char	*msg2;
 
+	msg = "\n\t ⏳To continue, please wait for your PID to be generated\t\n";
+	msg2 = "\n\tRun the client with the PID provided and a message to send \n";
+	ft_putstr_fd("\n\t\t\t\t MINITALK 🗯 \t\n", 1);
+	ft_putstr_fd(msg, 1);
 	pid = getpid();
-	ft_putstr_fd("\n\t\t\t\t MINITALK \t\n", 1);
-	ft_putstr_fd("\n\t To continue, please wait for your PID to be generated\t\n", 1);
 	sleep(1);
-	ft_putstr_fd("\n\t\t\t\t  PID: ", 1);
+	ft_putstr_fd("\n\t\t\t\tPID: ", 1);
 	ft_putnbr_fd(pid, 1);
 	ft_putstr_fd("\n", 1);
-	// sleep(2);
-	ft_putstr_fd("\n You can now run the client with the PID provided and a message to send \n", 1);
+	ft_putstr_fd(msg2, 1);
 	signal(SIGUSR1, convert_tochar);
 	signal(SIGUSR2, convert_tochar);
-	while (1);
+	while (1)
+	{
+		pause();
+	}
 }
+
